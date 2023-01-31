b5251d4d6af51bb34346e5c45d7f07136372a833
tom-choi <58120532+tom-choi@users.noreply.github.com>
2023-01-31 12:54:10 +0800

    03-scence import

--- Assets/Script/Controller/BaseButton.cs
+++ Assets/Script/Controller/BaseButton.cs
@@ -6,9 +6,16 @@ using UnityEngine.UI;
 
 public class BaseButton : MonoBehaviour
 {
-    public void ListClick()
+    public void ListClick(GameObject prefab)
     {
-        Debug.Log("Button绑定function的点击事件！");
+        Debug.Log("点击事件: 展示子資料夾");
+        //GameObject originalGameObject = Instantiate(prefab);
+        for (int i = 0; i < prefab.transform.childCount; i++)
+        {
+            GameObject child = prefab.transform.GetChild(i).gameObject;
+            //Do something with child
+            child.gameObject.SetActive(false);
+        }
     }
 
     // Update is called once per frame
